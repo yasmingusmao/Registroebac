5a37d9ca1bfafb2cf6b6c3e0e05a6e115c3594b8
Yasmin Gusmão <yagusmao@gmail.com>
2024-07-18 11:29:55 -0300

    Opção de saída
    Incluso opção de sair do sistema para usuário.

--- Registro_alunosEBAC.c
+++ Registro_alunosEBAC.c
@@ -147,6 +147,7 @@ int main ()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção: "); //fim do menu
 	
 		scanf("%d", &opcao); //armazenando a escolha do usuário na variável
@@ -165,6 +166,11 @@ int main ()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigada por utilizar nosso sistema!");
+			return 0;
 			break;
 					
 			default:
